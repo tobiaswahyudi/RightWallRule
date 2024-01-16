3dc9efa8d67217addf6b9a285f2f3e72072f4695
Tobias Wahyudi <tobi8800@gmail.com>
2024-01-16 03:47:34 -0500

    Player renders a hand drawn circle

--- entities/player.js
+++ entities/player.js
@@ -5,6 +5,7 @@ import { EFFECT_LAYERS } from "../effects/effect.js";
 import { CircleEffect } from "../effects/circleEffect.js";
 import { Vector2 } from "../utils/vector2.js";
 import gameEngine from "../core/engine.js";
+import { ImageSrc } from "../utils/image.js";
 
 export class Player extends Entity {
   constructor(gameEngine) {
@@ -17,6 +18,7 @@ export class Player extends Entity {
     gameEngine.spawnEffect(EFFECT_LAYERS.under, this.shadow, -1);
 
     this.lastDirection = new Vector2(1, 0);
+    this.image = ImageSrc('./img/entities/player.png');
   }
 
   followMe(xOffset, yOffset) {
@@ -33,7 +35,8 @@ export class Player extends Entity {
   }
 
   render(context) {
-    this.shape.render(context);
+    // this.shape.render(context);
+    context.drawImage(this.image, this.position.x - 27, this.position.y - 27);
     context.save();
     context.translate(this.position.x, this.position.y);
     context.rotate(this.lastDirection.theta);
